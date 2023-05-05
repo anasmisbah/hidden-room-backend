7cd45dbdcbb4f7318c4ad93f06a7cd7d7c9805b3
Anas Misbah <anas.misbahuddin4869@gmail.com>
2023-05-05 14:02:10 +0800

    api checkroom

--- routes/room/check/[room].dart
+++ routes/room/check/[room].dart
@@ -2,12 +2,29 @@ import 'dart:convert';
 import 'dart:io';
 
 import 'package:dart_frog/dart_frog.dart';
+import 'package:hidden_room_backend/repository/room_repository.dart';
 
-Future<Response> onRequest(RequestContext context,String room)async {
-    
-  return Response.json(body: {
-    'status':true,
-    'Message':'success',
-    'data':room,
-  });
+Future<Response> onRequest(RequestContext context, String room) async {
+  final response = await context.read<RoomRepository>().checkRoom(
+        roomCode: room.toUpperCase(),
+      );
+  print(response);
+  if (response == null) {
+    return Response.json(
+      body: {
+        'status': false,
+        'Message': 'room not found',
+        'data': null,
+      },
+      statusCode: HttpStatus.badRequest,
+    );
+  }
+
+  return Response.json(
+    body: {
+      'status': true,
+      'Message': 'success',
+      'data': response,
+    },
+  );
 }
